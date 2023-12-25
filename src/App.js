c4e97b4eb30cfe8e76e6327aff6f211c03b69ca4
joshi <joseph.ilagan.99@gmail.com>
2023-12-25 20:43:05 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import useCounter from './component/useCounter';
 
 function App() {
+ const [count, add, lessen, clear] = useCounter()
   return (
     <div className="App">
-      
+      <h1>Counter</h1>
+      <button onClick={add}>Increase</button>
+      <button onClick={lessen}>Decrease</button>
+      <button onClick={clear}>Set to Zero</button>
+      <p>{count}</p>
     </div>
   );
 }
